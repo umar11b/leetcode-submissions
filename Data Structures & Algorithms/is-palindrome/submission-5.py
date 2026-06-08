d7541118255e676c06d27ebd9c3f93438d6cf2d9
Umar Zaman <76013540+umar11b@users.noreply.github.com>
2026-06-07 21:08:27 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # attempt 2 from heart
+        # Initialize left and right pointers
+        l, r = 0, len(s) - 1
+
+        # Loop while left is less than right
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+        
+
+            
+
